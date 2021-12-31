65315d77e1cb62e003a0e8114c4eba6ca6074d8e
jimenezbaquero <36899305+jimenezbaquero@users.noreply.github.com>
2021-12-31 17:58:26 +0100

    ADD: roles and users seeder MODIFY: move user model to models directory

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +37,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function guardName(){
+        return "web";
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        Role::create(['name'=>'admin']);
+        Role::create(['name'=>'minero']);
+        Role::create(['name'=>'guest']);
+
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('1234'), // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user->assignRole('admin');
+
+
+        for($i=1;$i<100;$i++) {
+            $user = User::create([
+                'name' => 'user'.$i,
+                'email' => 'user'.$i.'@email.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('1234'), // password
+                'remember_token' => Str::random(10),
+            ]);
+            $user->assignRole('minero');
+        }
+
     }
 }
